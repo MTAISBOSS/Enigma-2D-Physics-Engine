aa617f971576a3d026fe926cd3d01eb548dd86c2
mtaisboss <mohammad.1387.kavian@gmail.com>
2026-01-28 19:53:00 +0330

    added rigidbody

---
+++ Physics-Engine/Physics-Engine/Core/World.cs
@@ -0,0 +1,13 @@
+﻿namespace Physics_Engine.Core
+{
+    public sealed class World
+    {
+        public static readonly float MinBodySize = 0.01f * 0.01f;
+        public static readonly float MaxBodySize = 64f * 64f;
+        
+        public static readonly float MinDensity  = 0.5f;
+        public static readonly float MaxDensity = 21.4f;
+        
+        
+    }
+}
